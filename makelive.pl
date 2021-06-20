203ac851fba114fec22233c59d63387724b3931c
Robert <robertkey@mighty.co.za>
2021-06-20 16:23:44 +0200

    added more code to sub partition1

--- makelive.pl
+++ makelive.pl
@@ -20,8 +20,11 @@ use warnings;
 #
 #######################################################
 
+#######################################################
 # this sub determines the version
 # which will be used for grub
+# parameters passed: getversion(full-path-to-iso)
+######################################################
 sub getversion {
 	################################
 	# determine the version for grub
@@ -57,21 +60,37 @@ sub getversion {
 # setparition1(chroot-directory, path-to-device)
 ####################################################
 sub setpartition1 {
-	my $chroot-dir = shift;
-	my $device = shift;
+	my ($ubuntuiso, $chroot_dir, $dev_path)  = @_;
+
+	# mount ubuntu iso image at /mnt/cdrom
+	system("mount " . $ubuntuiso . " /mnt/cdrom -o ro");
+	
+	# mount the dev under chroot/boot if not mounted
+	my $rc = system("grep " . $dev_path . "1 /etc/mtab");
+	system("mount -L MACRIUM " . $chroot_dir . "/boot") unless $rc == 0;
 	
 	# unsquash filesystem.squashfs to the chroot directory
-	system("unsquashfs -d $chroot-dir /mnt/cdrom/casper/filesystem.squashfs");
+	# the directory must not exist
+	system("unsquashfs -d $chroot_dir /mnt/cdrom/casper/filesystem.squashfs");
 	
 	# copy other files
-	system("cp /etc/resolv.conf /etc/hosts " . $chroot-dir . "/etc/");
+	system("cp /etc/resolv.conf /etc/hosts " . $chroot_dir . "/etc/");
+	system("cp /mnt/cdrom/casper/vmlinuz /mnt/cdrom/casper/initrd " . $chroot_dir . "/boot/");
+	
+	# un mount /mnt/cdrom
+	system("umount /mnt/cdrom");
 	
 	# copy etc/apt files
 	chdir "/etc/apt";
-	system("cp -a trustedgpg trusted.gpg.d sources.list " . $chroot-dir . "/etc/apt/");
+	system("cp -a trustedgpg trusted.gpg.d sources.list " . $chroot_dir . "/etc/apt/");
 	
 	# copy from subversion
-	system("svn --force export --depth files file:///mnt/svn/root/my-linux/livescripts " . $chroot-dir . "/usr/local/bin/");
+	system("svn --force export --depth files file:///mnt/svn/root/my-linux/livescripts " . $chroot_dir . "/usr/local/bin/");
+	system("svn --force export --depth files file:///mnt/svn/root/live/scripts/grub/vat/mbr/grub.cfg " . $chroot_dir . "/boot/grub/");
+    system("svn --force export --depth files file:///mnt/svn/root/live/scripts/grub/vat/efi/grub.cfg " . $chroot_dir . "/boot/EFI/grub/");
+    
+    # now edit grub.cfg with the new version
+    
 }
 ##################
 # Main entry point
@@ -81,13 +100,16 @@ sub setpartition1 {
 # makelive.pl ubuntuiso-name chroot-directory path-to-hdd partition-no
 # get command line argument
 # this is the name of the ubuntu iso image
-my $ubuntuiso = $ARGV[0];
+my ($ubuntuiso, $chroot_dir, $dev_path, $part_no) = @ARGV;
 
 my $version = getversion($ubuntuiso);
 
 print "$version\n";
 
-# mount ubuntu iso image at /mnt/cdrom
-system("mount " . $ubuntuiso . " /mnt/cdrom -o ro");
-
-
+# setup correct partition no
+if ($part_no == 1) {
+	# setup partition 1
+	setpartition1($ubuntuiso, $chroot_dir, $dev_path);
+} elsif ($part_no == 2) {
+	# setup partition 2
+}
