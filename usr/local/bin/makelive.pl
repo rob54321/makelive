bbbcc5282b626ca7704cf3c9af1abdda6855db92
Robert <robertkey@mighty.co.za>
2024-02-05 11:07:50 +0200

    added sub mountdevice

--- usr/local/bin/makelive.pl
+++ usr/local/bin/makelive.pl
@@ -44,6 +44,55 @@ our($opt_m, $opt_i, $opt_c, $opt_e, $opt_u, $opt_p, $opt_s, $opt_D, $opt_S, $opt
 #
 #######################################################
 
+
+###################################################
+# sub to mount devices
+# simple script to mount a device
+# if mounted , umount all locations.
+# then mount with the options
+# finish and klaar
+###################################################
+sub mountdevice {
+	# parameters
+	my $label = shift @_;
+	my $mtpt = shift @_;
+	my $options = shift @_;
+
+	# if no options given, use defaults
+	# which is rw
+	$options = "rw" unless ($options);
+
+	# check if device is mounted and if mounted multiple times
+	my @target = `findmnt --source LABEL=$label -o TARGET`;
+	chomp(@target);
+
+	# return codes
+	my $rc;
+
+	#@target = ("TARGET", mountpoint)
+	# it may be mounted at multiple locations
+	# one of the locations may or may not be correct
+	if (@target) {
+		# device is mounted at lease once
+
+		#get rid of header TARGET
+		shift @target;
+
+		# umount all mounts
+		foreach my $item (@target) {
+			# umount
+			$rc = system("umount -v $item");
+			die "Could not umount $label from $item: $!\n" unless $rc == 0;
+		}
+		# all mounts now un mounted for the device
+	}
+
+	# mount the device
+	$rc = system("mount -L $label -o $options $mtpt");
+	die "Could not mount $label at $mtpt -o $options: $!\n" unless $rc == 0;
+	print "mounted $label at $mtpt options: $options\n";
+}
+
 #######################################################
 # sub to restore /mnt/debhome and /mnt/svn links
 # in main system
