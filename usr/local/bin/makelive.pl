e45ad5bcd5428aebbdeffa0a16c70fee0114da95
Robert <robertkey@mighty.co.za>
2024-11-08 14:07:41 +0200

    determined kernel version and write to file

--- usr/local/bin/makelive.pl
+++ usr/local/bin/makelive.pl
@@ -1297,6 +1297,25 @@ sub createchroot {
 	
 	$rc = system("cp -dR .disk dists install pool preseed " . $chroot_dir . "/isoimage/");
 	die "could not copy dists install pool preseed to $chroot_dir/isoimage: $!\n" unless $rc == 0;
+	
+	# get the kernel version from file
+	# chroot_dir/isoimage/pool/main/l/linux-signed/linux-image-XXXXXX-generic_amd64.deb
+	# and save it to chroot_dir/
+	my $kernelfile = `ls $chroot_dir/isoimage/pool/main/l/linux-signed`;
+
+	# get version from file linux-image-XXXXXX-generic
+	$kernelfile =~ /linux-image-(.*)-generic/;
+	my $kernelversion;
+	if (defined($1)) {
+		$kernelversion = $1;
+	} else {
+		die "Could not determine kernel version\n";
+	}
+	
+	# save kernversion to chroot_dir/isoimage/kernelversion.txt
+	open FH, ">", "$chroot_dir/isoimage/kernelversion.txt";
+	print FH $kernelversion . "\n";
+	close FH;
 
 	# save the version and codename of linux
 	saveversioncodename ();
